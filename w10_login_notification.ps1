e83166fc9af5b0c03f985733f61526f4c55c8df1
RedFoxFinn <31569997+RedFoxFinn@users.noreply.github.com>
2020-11-05 10:17:43 +0200

    Added the ps1-script

---
+++ w10_login_notification.ps1
@@ -0,0 +1,16 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$deviceName = $env:ComputerName
+$userName = $env:USERNAME
+
+$notificationContent = 'User: ' + $currentUser + "`n" + 'Device: ' + $deviceName
+
+$global:balloon = New-Object System.Windows.Forms.NotifyIcon
+$path = (Get-Process -id $pid).Path
+$balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
+$balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::None
+$balloon.BalloonTipText = 'Login information'
+$balloon.BalloonTipTitle = $notificationContent 
+$balloon.Visible = $true 
+$balloon.ShowBalloonTip(60000)
